f5469c738384509d18e72956b541552c82039291
Rayan Syed <66187079+rayan-syed@users.noreply.github.com>
2023-12-10 12:31:32 -0500

    Update baud_rate_generator.v

--- Source Code/UART/baud_rate_generator.v
+++ Source Code/UART/baud_rate_generator.v
@@ -1,15 +1,11 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Reference Book: FPGA Prototyping By Verilog Examples Xilinx Spartan-3 Version
-// Authored by: Dr. Pong P. Chu
-// Published by: Wiley
+// 
 //
-// Adapted for the Basys 3 Artix-7 FPGA by David J. Marion
+// Code adapted from https://www.youtube.com/watch?v=L1D5rBwGTwY, FPGA DUDE
 //
-// Baud Rate Generator for the UART System
 //
-// Comments:
-// - Many of the variable names have been changed for clarity
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 module baud_rate_generator
