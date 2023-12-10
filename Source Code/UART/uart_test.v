3a6a9d1187774a65870a0c1391647f5355ed50ea
Rayan Syed <66187079+rayan-syed@users.noreply.github.com>
2023-12-10 12:36:59 -0500

    Update uart_test.v

--- Source Code/UART/uart_test.v
+++ Source Code/UART/uart_test.v
@@ -1,15 +1,9 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Reference Book: FPGA Prototyping By Verilog Examples Xilinx Spartan-3 Version
-// Authored by: Dr. Pong P. Chu
-// Published by: Wiley
+// 
 //
-// Adapted for the Basys 3 Artix-7 FPGA by David J. Marion
+//Code adapted from https://www.youtube.com/watch?v=L1D5rBwGTwY, FPGA DUDE
 //
-// UART System Verification Circuit
-//
-// Comments:
-// - Many of the variable names have been changed for clarity
 //////////////////////////////////////////////////////////////////////////////////
 
 module uart_test(
@@ -23,9 +17,9 @@ module uart_test(
     );
     
     // Connection Signals
-    wire rx_full, rx_empty;
+    wire rx_full, rx_empty; //to display on 7 segment to help keep track of whats going on
     
-    //encrypt in middle of fifos
+    //variables encrypt in middle of fifos
     wire [63:0] encrypt_in, encrypt_out;
     
     // Complete UART Core
@@ -33,13 +27,13 @@ module uart_test(
         (
             .clk_100MHz(clk_100MHz),
             .reset(reset),
-            .encrypt(btn_tick),
-            .rx(rx),
-            .tx(tx),
-            .rx_full(rx_full),
-            .rx_empty(rx_empty),
-            .read_data(encrypt_in),
-            .write_data(encrypt_out)
+            .encrypt(btn_tick),     //encrypt when button pressed
+            .rx(rx),                //receieved data
+            .tx(tx),                //transmitted data
+            .rx_full(rx_full),      //buffer full for 7 segment
+            .rx_empty(rx_empty),    //buffer empty for 7 segment
+            .read_data(encrypt_in),     //data into output buffer
+            .write_data(encrypt_out)    //data from input buffer
         );
         
     // Button Debouncer
@@ -49,12 +43,12 @@ module uart_test(
             .reset(reset),
             .btn(btn),         
             .db_level(),  
-            .db_tick(btn_tick)
+            .db_tick(btn_tick)  //the button will start encoding so needs to be debounced
         );
     
     //display if buffer full or empty    
     assign an = 8'b11111110;        // using only one 7 segment digit 
-    assign seg = {~rx_full, 2'b11, ~rx_empty, 3'b111};      //line down if empty, line up if full
+    assign seg = {~rx_full, 2'b11, ~rx_empty, 3'b111};      //line down if empty, line up if full, only can click button when full
     
     //ENCRYPTION PERFORMED HERE
     assign encrypt_in = encrypt_out;
