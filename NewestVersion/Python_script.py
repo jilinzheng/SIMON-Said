85eb4045a073132535292650b75e7a29d7175883
Jilin <133818802+jilinzheng@users.noreply.github.com>
2023-12-11 22:21:00 -0500

    Update Python_script.py

--- NewestVersion/Python_script.py
+++ NewestVersion/Python_script.py
@@ -1,6 +1,11 @@
+from datetime import datetime
+
 def ROTL32(value, shift):
     return ((value << shift) & 0xFFFFFFFF) | (value >> (32 - shift))
 
+def ROTR32(value, shift):
+    return ((value >> shift)) | (0xFFFFFFFF & value << (32 - shift))
+    #return (((x >> r) & 0xFFFFFFFF) | (x << (32-r)))
 
 def f32(x):
     return ((ROTL32(x, 1) & ROTL32(x, 8)) ^ ROTL32(x, 2))
@@ -22,13 +27,17 @@ def Simon6496KeySchedule(K):
     rk[1] = K[1]
     rk[2] = K[2]
     for i in range(3, 42):
-        rk[i] = c ^ (z & 1) ^ rk[i - 3] ^ ROTL32(rk[i - 1], 3) ^ ROTL32(rk[i - 1], 4)
-        z >>= 1 
-    
+        rk[i] = c ^ (z & 1) ^ rk[i - 3] ^ ROTR32(rk[i - 1], 3) ^ ROTR32(rk[i - 1], 4)
+        z >>= 1
+        #print("Round " + str(i) + ": ", end="")
+        #print(hex(rk[i]))
+    '''
     for i in range(0, 42):
-     print(hex(rk[i]))
+        print("Round " + str(i) + ": ", end="")
+        print(hex(rk[i]))
+    '''
     return rk
-
+    
 
 def Simon6496Encrypt(Pt, rk):
     #print(Pt, rk)
@@ -41,7 +50,7 @@ def Simon6496Encrypt(Pt, rk):
         i += 2 
     
     #print(Ct)
-    return "{:08X}{:08X}".format(Ct[0], Ct[1])
+    return "{:08X}{:08X}".format(Ct[1], Ct[0])
 
 def Simon6496Decrypt(Pt, Ct, rk):
     Pt[1] = Ct[1]
@@ -51,9 +60,13 @@ def Simon6496Decrypt(Pt, Ct, rk):
         Pt[0], Pt[1] = R32x2(Pt[0], Pt[1], rk[i], rk[i - 1])
         i -= 2
 
-Key = [0x13121110, 0x0b0a0908, 0x03020100]
-PT = [0x6f722067, 0x6e696c63]
+startTime = datetime.now()
+#Key = [0x13121110, 0x0b0a0908, 0x03020100]
+Key = [0x03020100, 0x0b0a0908, 0x13121110]
+PT = [0x6e696c63, 0x6f722067]
 rk = Simon6496KeySchedule(Key)
 CT = Simon6496Encrypt(PT, rk)
 print(CT)
+print("Time elapsed: ", end = "")
+print(datetime.now() - startTime)
 #output should be: 5ca2e27f111a8fc8
